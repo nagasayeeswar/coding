e47096afefeecbd187dd883f84df6cfab3bd1738
Nagasayeeswar <naga4.eeswar@gmail.com>
2019-06-05 13:44:43 +0530

    Pointers Program finding sum,difference,product etc

--- pointers.c
+++ pointers.c
@@ -2,13 +2,19 @@
 void main()
 {
 int a=5,b=6;
-int *c;
-c=&a;
-printf("\na=%d, b=%d, c=%d\n", a, b, *c);
-*c=10;
+int *c, *d;
+c=&a,d=&b;
+printf("\na=%d, b=%d, c=%d, d=%d\n", a, b, *c, *d);
+*c=10, *d=8;
 
-printf("\na=%d, b=%d, c=%d\n", a, b, *c);
+printf("\na=%d, b=%d, c=%d, d=%d\n", a, b, *c, *d);
+int Sum=*c+*d;
+int Difference=*c-*d;
+int Product=*c * *d;
+float Division=*c / *d;
 
+
+printf("\nSum=%d,\nDifference=%d,\nProduct=%d,\nDivision=%f",Sum, Difference ,Product,Division);
 
 
 
