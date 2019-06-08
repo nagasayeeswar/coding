7c08e4b55eb9a33168dcd0ccf25183c502540917
Nagasayeeswar <naga4.eeswar@gmail.com>
2019-06-08 10:09:04 +0530

    Celcius to farenheit check

--- CtoF.c
+++ CtoF.c
@@ -14,4 +14,45 @@ void main()
 	scanf("%d",&celcius);
 	float farenheit=get_farenheit(celcius);
 	printf("Temperature in Farenheit is f=%0.3f",farenheit);
+	if (get_farenheit(42)==107.6f)
+	{
+		printf("\nCorrect");
+	}
+	else
+	{	
+	printf("\nError");
+	}
+	
+	if (get_farenheit(40)==104.0f)
+	{
+		printf("\nCorrect");
+	}
+	else
+	{	
+	printf("\nError");
+	}
+	if (get_farenheit(38)==100.4f)
+	{
+		printf("\nCorrect");
+	}
+	else
+	{	
+	printf("\nError");
+	}
+	if (get_farenheit(36)==96.8f)
+	{
+		printf("\nCorrect");
+	}
+	else
+	{	
+	printf("\nError");
+	}
+	if (get_farenheit(35)==96.0f)
+	{
+		printf("\nCorrect");
+	}
+	else
+	{	
+	printf("\nError");
+	}
 }
